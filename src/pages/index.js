c12082909acbee2f5141031f4284b29da53df959
Sadit-Shah <144421061+Sadit-Shah@users.noreply.github.com>
2023-09-10 16:56:22 +0000

    vidl embd

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,9 +31,7 @@ export default function Home() {
       </Head>
       
        <div className={styles.slider} id="player">
-       <iframe id="ytplayer" type="text/html" width="100%" height="100%"
-          src="https://www.youtube.com/watch?v=1SiUArkp0dU&amp;loop=1&amp;autoplay=1&amp;mute=1&amp;controls=0&amp;modestbranding=1"
-  frameBorder="0" allowFullScreen></iframe> 
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/1SiUArkp0dU?si=9TPunjmOVwE6VHlp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
 
 
