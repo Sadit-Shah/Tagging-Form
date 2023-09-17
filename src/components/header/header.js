a9b29b283a8fa3f5ff6baf7e9f491b17cc08c8bf
Sadit-Shah <144421061+Sadit-Shah@users.noreply.github.com>
2023-09-17 09:37:32 +0000

    logonew

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -19,7 +19,7 @@ export default function Header() {
       <div className={headercss.header}>
         <div className={headercss.logo}>
           {/* <Image className={headercss.logoimg} src="/images/logo.png" alt='headerimg'></Image> */}
-          <img className={headercss.logoimg} src="/images/logo.png" loading="lazy"/>
+          <img className={headercss.logoimg} src="/images/newlogo.jpeg" loading="lazy"/>
 
         </div>
         <div className={`${headercss.navbar} ${sideMenu ? `${headercss.showMenu}` : ""}`}>
