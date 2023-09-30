187a7fca67e60b51e6f9f423dea7ee5bd5852ca6
Sadit-Shah <144421061+Sadit-Shah@users.noreply.github.com>
2023-09-30 10:53:43 +0000

    css check

--- src/pages/contactus/contactus.js
+++ src/pages/contactus/contactus.js
@@ -1,12 +1,13 @@
 import cstyle from './contactus.module.scss'
-
+import contactuscss from './contactus.module.scss';
+import contactcss from './contactus.module.scss'
 export default function contactus() {
     return (
         <>
 
 
-       <div className='contactform'>
-          <div className='heading'>
+       <div className={contactcss.contactform}>
+          <div className={contactcss.heading}>
           <h3>
           Our support team will get back to you ASAP via email.
           </h3>
