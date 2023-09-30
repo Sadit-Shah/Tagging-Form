2e393276333ae03bd9ae8340e21c540fbee84699
Sadit-Shah <144421061+Sadit-Shah@users.noreply.github.com>
2023-09-30 10:02:32 +0000

    conty

--- src/pages/contactus/contactus.js
+++ src/pages/contactus/contactus.js
@@ -3,65 +3,7 @@ import cstyle from './contactus.module.scss'
 export default function contactus() {
     return (
         <>
-        <div>
-        <form className={container}>
-        <h1>Get in touch</h1>
-        <div className={email}>
-          <label htmlFor="frm-email">Email</label>
-          <input
-            id="frm-email"
-            type="email"
-            name="email"
-            autoComplete="email"
-            required
-          />
-        </div>
-        <div className={phone}>
-          <label htmlFor="frm-phone">Phone</label>
-          <input
-            id="frm-phone"
-            type="text"
-            name="phone"
-            autoComplete="tel"
-            required
-          />
-        </div>
-        <div className={fname}>
-          <div>
-            <label htmlFor="frm-first">First Name</label>
-            <input
-              id="frm-first"
-              type="text"
-              name="first"
-              autoComplete="given-name"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="frm-last">Last Name</label>
-            <input
-              id="frm-last"
-              type="text"
-              name="last"
-              autoComplete="family-name"
-              required
-            />
-          </div>
-        </div>
-        <div className={message}>
-          <label htmlFor="frm-message">Message</label>
-          <textarea id="frm-message" rows="6" name="message"></textarea>
-        </div>
-        <div className={button}>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-      </div>
-            
-
-
-
-
+       
       </>
 
 
