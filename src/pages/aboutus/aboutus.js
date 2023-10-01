d1a497b598a6c9fd22ff363b2800b9450dc408f7
Sadit Shah <saditshah@gmail.com>
2023-10-01 17:22:45 +0530

    final accpt

--- src/pages/aboutus/aboutus.js
+++ src/pages/aboutus/aboutus.js
@@ -2,16 +2,13 @@
 import Enquiry from "@/components/enquiry/enquiry"
 import Trendingcard from "@/components/trendingcard/trendingcard"
 export default function aboutus() {
-  return <>
-<<<<<<< HEAD
+  
+  return (<>
+
   <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
    <Trendingcard/>
 </div>
-=======
-  
-
->>>>>>> 61a8fb6204a27b98bbc65bd397ef33d6177afb9b
   </>
-
+  )
 }
 
