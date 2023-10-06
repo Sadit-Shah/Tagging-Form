e9daa7963b10201b473a480a15a404060b5c2be1
Sadit-Shah <144421061+Sadit-Shah@users.noreply.github.com>
2023-10-06 14:26:33 +0000

    trending

--- src/components/trendingcard/trendingcard.js
+++ src/components/trendingcard/trendingcard.js
@@ -1,11 +1,18 @@
 
 import Image from 'next/image'
 import Trendingcardcss from './trendingcard.module.scss'
+import Aos from 'aos'
+import { useEffect } from 'react';
 
 export default function Trendingcard(props) {
+    useEffect(() => {
+        Aos.init({
+          duration: 2000
+        });
+      })
     return (
         <>
-            <div className={Trendingcardcss.cardcont}>
+            <div data-aos="fade-up" className={Trendingcardcss.cardcont}>
                 <div className={Trendingcardcss.imgcont}>
                     <Image src={props.imageSrc} width={200} height={330} />
                 </div>
