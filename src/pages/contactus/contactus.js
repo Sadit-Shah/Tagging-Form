baad14baa943c3fd8dc656c6b8036eff826eec25
Sadit-Shah <144421061+Sadit-Shah@users.noreply.github.com>
2023-10-12 07:45:04 +0000

    formcn

--- src/pages/contactus/contactus.js
+++ src/pages/contactus/contactus.js
@@ -3,58 +3,49 @@ export default function contactus() {
     return (
         <>
             <div className={contactcss.contactcont} >
-                <div className={contactcss.address}>
-                    <div className={contactcss.location}>
-                        <div className={contactcss.locicon}>
-                                <span>
-                                <i class="fa-sharp fa-solid fa-location-dot"></i>
-                                </span>
-                        </div>
-                        <div className={contactcss.locdesc}>
-                            <span>Address</span>
-                        </div>
+                <div className={contactcss.details}>
+                    <div className={contactcss.icon}>
+                        <i class="fa-sharp fa-solid fa-location-dot"></i>
+                    </div>
+                    <div className={contactcss.address}>
+                        <span>Address</span>
+                        <span className={contactcss.subtext}>Address</span>
+                    </div>
+                    <div>
 
                     </div>
-                    {/* <span>
-                        <i class="fa-sharp fa-solid fa-location-dot"></i>
-                    </span>
-                    <div className={contactcss.desc}>
-                        <h3>Address</h3>
-                    
-                      <br/>
-                      <span>Ellahibagh, Hazratbal Road,<br/>Near Grid Station-190020</span>
-                      </div> */}
+
                 </div>
                 <div className={contactcss.form}>
-                      <span className={contactcss.title}>Send Us A Message </span>
-                      <label> TELL US YOUR NAME *</label> 
-                      <div className={contactcss.name} >
-                            <input className={contactcss.textbox} placeholder='First Name'></input>
-                            <input className={contactcss.textbox} placeholder='Last Name'></input>
-                      </div>
+                    <span className={contactcss.title}>Send Us A Message </span>
+                    <label> TELL US YOUR NAME *</label>
+                    <div className={contactcss.name} >
+                        <input className={contactcss.textbox} placeholder='First Name'></input>
+                        <input className={contactcss.textbox} placeholder='Last Name'></input>
+                    </div>
 
-                      <label>ENTER YOUR EMAIL *</label> 
-                      <div className={contactcss.email} >
-                            <input className={contactcss.textbox} placeholder='Eg. example@gmail.com'></input>
-                           
-                      </div>
-                      <label>ENTER YOUR PHONE NUMBER *</label> 
-                      <div className={contactcss.email} >
-                            <input className={contactcss.textbox} placeholder='9906000000'></input>
-                           
-                      </div>
-                      <label>MESSAGE *</label> 
-                      <div className={contactcss.message} >
-                            <textarea className={contactcss.msg} placeholder='Write us a message'></textarea>
-                           
-                      </div>
-                      <div className={contactcss.button}>
-                 <input type="button" value="Send Message" />
+                    <label>ENTER YOUR EMAIL *</label>
+                    <div className={contactcss.email} >
+                        <input className={contactcss.textbox} placeholder='Eg. example@gmail.com'></input>
 
-                 </div>
+                    </div>
+                    <label>ENTER YOUR PHONE NUMBER *</label>
+                    <div className={contactcss.email} >
+                        <input className={contactcss.textbox} placeholder='9906000000'></input>
+
+                    </div>
+                    <label>MESSAGE *</label>
+                    <div className={contactcss.message} >
+                        <textarea className={contactcss.msg} placeholder='Write us a message'></textarea>
+
+                    </div>
+                    <div className={contactcss.button}>
+                        <input type="button" value="Send Message" />
+
+                    </div>
                 </div>
-                 
-                 
+
+
 
             </div>
 
@@ -62,35 +53,6 @@ export default function contactus() {
 
 
 
-            {/* <div className={contactcss.contactcontainer}>
-            <div className={contactcss.contactform}>
-                <div className={contactcss.heading}>
-                    <h3> Our support team will get back to you ASAP via email. </h3>
-                </div>
-                <div classN ame={contactcss.details}>
-                    <div className={contactcss.fname}>
-                    <label>First Name</label>
-                    <input type='text' placeholder='Enter Your First Name'></input>
-                    </div>
-
-                    <div className={contactcss.fname}>
-                    <label>Middle Name</label>
-                    <input type='text' placeholder='Enter Your Middle Name'></input>
-                    </div>
-
-                    <div className={contactcss.fname}>
-                    <label>Last Name</label>
-                    <input type='text' placeholder='Enter Your Last Name'></input>
-                    </div>
-
-
-            </div>
-
-
-
-        </div >
-
-        </div> */}
         </>
 
 
