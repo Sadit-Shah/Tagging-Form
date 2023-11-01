25467ae776c8837fa302177f9ba699ed9ddfb2ce
Sadit-Shah <144421061+Sadit-Shah@users.noreply.github.com>
2023-11-01 09:08:50 +0000

    termsncond

---
+++ src/components/termsandcond/termsandcond.js
@@ -0,0 +1,95 @@
+import termscss from './termsandcond.module.scss'
+export default function Termsandcond() {
+    return (
+        <div className={termscss.container}>
+            <h3>
+                TERMS AND CONDITIONS
+            </h3>
+            <div className={termscss.heading}>
+                <span>
+                    CANCELLATION POLICY
+                </span>
+            </div>
+            <p>
+                Once a partial or full payment has been made,
+                cancellations must be submitted in writing to OPENSKYTRIPS@GMAIL.COM.
+                Cancellation fees will be applied based on the date that the written
+                cancellation is received and based on the following days prior to departure.
+                Fees will be calculated as a percentage of the total amount received excluding ticketed airfare.
+                Once a partial or full payment has been made, cancellations
+                must be submitted in writing to OPENSKYTRIPS@GMAIL.COM.
+                Cancellation fees will be applied based on the date that the written
+                cancellation is received and based on the following days prior to departure.
+                Fees will be calculated as a percentage of the total amount received excluding ticketed airfare.
+            </p>
+
+            <div className={termscss.heading}>
+                <span>
+                    FLIGHT CANCELLATION POLICY
+                </span>
+            </div>
+            <div className={termscss.lists}>
+                <ul>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i> As Per the Airline's Cancellation Policy.
+                    </li>
+                </ul>
+
+            </div>
+
+
+            <div className={termscss.heading}>
+                <span>
+                    LAND PACKAGE  CANCELLATION POLICY
+                </span>
+            </div>
+            <div className={termscss.lists}>
+                <ul>
+
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Processing fee: 13% of the total package cost will be charged regardless of any reason if you choose to cancel your trip post booking.</li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Between 45 days to 60 days from the trip start date: 25% of total package cost will be deducted as cancellation charge.</li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Between 44 to 31 days from the trip start date: 50% of total package cost will be deducted from the total package amount.</li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Between 30 to 21 days from the trip start date: 60% of total package cost will be deducted from the total package amount.</li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Between 20 and 14 days from the trip start date: 75% of total package cost will be deducted from the total package amount.</li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>In case of no-show, booking stands non-refundable.</li>
+
+
+
+                </ul>
+
+
+            </div>
+
+
+            <div className={termscss.heading}>
+                <span>
+                    ADDITIONAL INFORMATION
+                </span>
+            </div>
+            <div className={termscss.lists}>
+                <ul>
+
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Pre-paid sim cards don’t work in J&K due to security reasons, postpaid connection is a must.</li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>In Katra for Pithu / Palki / Pony services Municipal Committee Katra has fixed the rates and they are displayed on the rate card outside their booking office at Banganga.
+                        If someone is over charging above the listed rates it’s up to you how hard you can bargain.</li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>In Srinagar if you are staying at the Houseboat: - Shikara Ride are complimentary, do not pay for the same.</li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>It is not mandatory for guest to take ponies in Gulmarg & Pahalgam to visit all the important tourist spots, all the major tourist attraction point is in close proximity so you can walk it down. However, if still you wish to take pony ride / sledging.</li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Rafting etc the rates the highly negotiable. Please bargain to your max. </li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Sightseeing to Aru Valley / Betaab Valley / Chandanwari at Pahalgam will be by local taxi union only. Due to union outside vehicles cannot enter these places. The rate is highly negotiable. </li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>If you wish to visit these places. You have to pay extra directly.   </li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>For Shopping: - Dry Fruits / Rajma / Basmati Rice / Handicraft / Woolens / Carpets/ Saffron / Cricket Bats Srinagar:- Lal Chowk / Boulevard  </li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>For Kashmiri Wazwan Food:- Aadhoos at Lal Chowk Srinagar is the best place and Mutton Kebab at Gee Enn Bakers - Residency Road.    </li>
+                    <li><i class="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>For Vegetarian Food in Srinagar:- Krishna Dhaba is the best place. 9.) One day one destination will be covered   </li>
+
+
+                </ul>
+
+
+            </div>
+
+
+
+
+
+        </div>
+    )
+}
--- src/pages/contactus/contactus.js
+++ src/pages/contactus/contactus.js
@@ -1,11 +1,12 @@
 
 import Defective from '@/components/defective/defective'
+import Termsandcond from '@/components/termsandcond/termsandcond'
 export default function contactus() {
     return (
         <>
 
             <div>
-                <Defective/>
+                <Termsandcond/>
             </div>
         </>
     )
